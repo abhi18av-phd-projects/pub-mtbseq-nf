7483c80eccc00c33fa98c7796c3a35548fb5cbee
Abhinav Sharma <abhi18av@gmail.com>
2024-06-13 16:27:06 +0200

    add more tsv files

---
+++ compareResults.ps1
@@ -0,0 +1,47 @@
+
+#=========================
+## Modal comparison with baseline MTBseq results
+#=========================
+
+# MTBseq/CONDA vs  MTBSeq-nf/NORMAL
+
+#### AMEND
+
+delta `
+    mtbseq-v110-conda-baseline/Amend/NONE_joint_cf4_cr4_fr75_ph4_samples91_amended.tab `
+    mtbseq-nf-normal-1/Amend/mtbseqnf_joint_cf4_cr4_fr75_ph4_samples91_amended.tab  
+
+#### CLASSIFICATION
+
+delta `
+    mtbseq-v110-conda-baseline/Classification/Strain_Classification.tab `
+    mtbseq-nf-normal-1/Classification/Strain_Classification.tab      
+
+
+#### GROUPS
+
+delta `
+    mtbseq-v110-conda-baseline/Groups/NONE_joint_cf4_cr4_fr75_ph4_samples91_amended_u95_phylo_w12.matrix `
+    mtbseq-nf-normal-1/Groups/mtbseqnf_joint_cf4_cr4_fr75_ph4_samples91_amended_u95_phylo_w12.matrix
+
+#### JOINT
+#### STATISTICS
+
+
+
+# MTBseq/CONDA vs  MTBSeq-nf/PARALLEL
+
+#=========================
+## Intermodal comparison of consistency results 
+#=========================
+
+# MTBseq-nf/NORMAL vs  MTBSeq-nf/PARALLEL
+
+
+#=========================
+## Intramodal comparison for reproducibility
+#=========================
+
+# MTBseq-nf/NORMAL-1 vs  MTBseq-nf/NORMAL-2
+
+
